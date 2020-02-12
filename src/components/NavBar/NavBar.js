1ccfaa028b9b160a41eede012eaa3634c72611c3
Sergey <in43sh@gmail.com>
2020-02-11 22:14:24 -0800

    Start working on NavBar.js .

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <div>
+            <div>
+                
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
