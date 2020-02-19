e309a00b2e505e2250e40489b8faa6a290b1e57d
Alex <usalex93@gmail.com>
2020-02-18 17:41:09 -0800

    added variables for colors

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,9 +3,13 @@ import React from 'react';
 function Home() {
     return (
         <main>
-            <section class="section-slideshow">
+            <section className="section-slideshow">
             </section>
-            
+            <div className="banner">
+                <h1 className="banner-ad">More Than 2 Millions Items Available In Store</h1>
+                <button className="btn-store">
+                </button>
+            </div>
         </main>
     );
 }
