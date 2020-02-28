f57644afe9ee1ff1033b260133d9b56642fa1059
Alex <usalex93@gmail.com>
2020-02-27 20:17:48 -0800

    Categories layout is ready

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,13 +3,42 @@ import React from 'react';
 function Home() {
     return (
         <main>
-            <section className="section-slideshow">
+            <section className="section-categories">
+                <h2 className="heading-secondary">
+                    Categories
+                </h2>
+                <div className="categories-cont">
+                    <div className="categories-cont__item">
+                        <p className="categories-cont__item-cat">Categories</p>
+                    </div>
+                    <div className="categories-cont__item">
+                        <p className="categories-cont__item-cat">Categories</p>
+                    </div>
+                    <div className="categories-cont__item">
+                        <p className="categories-cont__item-cat">Categories</p>
+                    </div>
+                    <div className="categories-cont__item">
+                        <p className="categories-cont__item-cat">Categories</p>
+                    </div>
+                    <div className="categories-cont__item">
+                        <p className="categories-cont__item-cat">Categories</p>
+                    </div>
+                    <div className="categories-cont__item">
+                        <p className="categories-cont__item-cat">Categories</p>
+                    </div>
+                    <div className="categories-cont__item">
+                        <p className="categories-cont__item-cat">Categories</p>
+                    </div>
+                    <div className="categories-cont__item">
+                        <p className="categories-cont__item-cat">Categories</p>
+                    </div>
+                </div>
             </section>
-            <div className="banner">
+            {/* <div className="banner">
                 <h1 className="heading-primary">More Than 2 Millions Items Available In Store</h1>
                 <button className="btn-store"> Store
                 </button>
-            </div>
+            </div> */}
             <section className="section-popular-products">
                 <h2 className="heading-secondary">
                     Popular Products
@@ -27,6 +56,33 @@ function Home() {
                     <div className="products-cont__item">
 
                     </div>
+
+                    <div className="products-cont__item">
+
+                    </div>
+                    <div className="products-cont__item">
+
+                    </div>
+                    <div className="products-cont__item">
+
+                    </div>
+                    <div className="products-cont__item">
+
+                    </div>
+
+                    <div className="products-cont__item">
+
+                    </div>
+                    <div className="products-cont__item">
+
+                    </div>
+                    <div className="products-cont__item">
+
+                    </div>
+                    <div className="products-cont__item">
+
+                    </div>
+
                 </div>
             </section>
         </main>
