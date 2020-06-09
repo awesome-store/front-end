5f1dc04503f6718d0f422573690751880a7523c0
Sergey <in43sh@gmail.com>
2020-06-09 15:08:56 -0700

    Work on search bar on Home Page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 function Home() {
     return (
-        <main>
+        <main className="main">
+            <div className="search-bar">
+                <div className="search-bar__tag-item pointer">
+                    <span>Doors</span>
+                </div>
+                <div className="search-bar__tag-item pointer">
+                    <span>Furniture</span>
+                </div>
+                <div className="search-bar__tag-item pointer">
+                    <span>Decore</span>
+                </div>
+                <div className="search-bar__tag-item pointer">
+                    <span>Building material</span>
+                </div>
+                <div className="search-bar__tag-item pointer">
+                    <span>Lawn</span>
+                </div>
+                <div className="search-bar__tag-item pointer">
+                    <span>Ceiling Fans</span>
+                </div>
+                <div className="search-bar__tag-item pointer">
+                    <span>Garden</span>
+                </div>
+                <div className="search-bar__tag-item pointer">
+                    <span>Bath & Faucets</span>
+                </div>
+                <input placeholder="Search"/>
+            </div>
             <section className="section-categories">
                 <h2 className="heading-secondary">
                     Categories
