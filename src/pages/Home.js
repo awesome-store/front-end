09a5a071b6b8dae0b5fcdc7946b037f9cfb0a60c
Sergey <in43sh@gmail.com>
2020-06-10 00:37:44 -0700

    Work on home responsiveness.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,32 +8,32 @@ import toolkit from "../img/toolkit.png";
 function Home() {
     return (
         <main className="main">
-            <div className="search-bar">
-                <div className="search-bar__tag-item pointer">
+            <div className="search-bar-container">
+                <div className="search-bar-container__tag-item pointer">
                     <span>Doors</span>
                 </div>
-                <div className="search-bar__tag-item pointer">
+                <div className="search-bar-container__tag-item pointer">
                     <span>Furniture</span>
                 </div>
-                <div className="search-bar__tag-item pointer">
+                <div className="search-bar-container__tag-item pointer">
                     <span>Decore</span>
                 </div>
-                <div className="search-bar__tag-item pointer">
+                <div className="search-bar-container__tag-item pointer">
                     <span>Building material</span>
                 </div>
-                <div className="search-bar__tag-item pointer">
+                <div className="search-bar-container__tag-item pointer">
                     <span>Lawn</span>
                 </div>
-                <div className="search-bar__tag-item pointer">
+                <div className="search-bar-container__tag-item pointer">
                     <span>Ceiling Fans</span>
                 </div>
-                <div className="search-bar__tag-item pointer">
+                <div className="search-bar-container__tag-item pointer">
                     <span>Garden</span>
                 </div>
-                <div className="search-bar__tag-item pointer">
+                <div className="search-bar-container__tag-item pointer">
                     <span>Bath & Faucets</span>
                 </div>
-                <input placeholder="Search"/>
+                <input className="search-bar-container__search-bar" placeholder="Search"/>
             </div>
             <div className="top-banner">
                 <span>top banner</span>
