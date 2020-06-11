f853fff079d1729c2ef200542ab2cab93210e003
Sergey <in43sh@gmail.com>
2020-06-11 00:27:29 -0700

    Add tutorial-banner.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import screwdrivers from "../img/screwdrivers.jpg";
 import toolkit from "../img/toolkit.png";
 import vacuum from "../img/vacuum.png";
 import card from "../img/card.png";
+import flowers from "../img/flowers.png";
 
 function Home() {
     return (
@@ -63,26 +64,37 @@ function Home() {
                     <Product img={drill}/>
                 </div>
             </section>
-            <div className="home-page__banners-container">
-                <div className="small-banner">
-                    <div  className="small-banner__text-container small-banner__text-container--purple">
-                        <span className="small-banner__header">50% Off Select Dyson Vacuums</span>
-                        <span className="small-banner__description">Free delivery. Today only</span>
+            <section className="home-page__banners-container">
+                <div className="promotionnal-banner-small">
+                    <div  className="promotionnal-banner-small__text-container promotionnal-banner-small__text-container--purple">
+                        <span className="promotionnal-banner-small__header">50% Off Select Dyson Vacuums</span>
+                        <span className="promotionnal-banner-small__description">Free delivery. Today only</span>
                     </div>
-                    <div className="small-banner__img-container">
-                        <img className="small-banner__img" src={vacuum} alt="vacuum"/>
-                    </div>
-                </div>
-                <div className="small-banner">
-                    <div className="small-banner__text-container small-banner__text-container--blue">
-                        <span className="small-banner__header">Financing Available</span>
-                        <span className="small-banner__description">Up to 24 month financing. Terms & conditions apply</span>
-                    </div>
-                    <div className="small-banner__img-container">
-                        <img className="small-banner__img" src={card} alt="card"/>
+                    <div className="promotionnal-banner-small__img-container">
+                        <img className="promotionnal-banner-small__img" src={vacuum} alt="vacuum"/>
                     </div>
                 </div>
-            </div>
+                <div className="promotionnal-banner-small">
+                    <div className="promotionnal-banner-small__text-container promotionnal-banner-small__text-container--blue">
+                        <span className="promotionnal-banner-small__header">Financing Available</span>
+                        <span className="promotionnal-banner-small__description">Up to 24 month financing. Terms & conditions apply</span>
+                    </div>
+                    <div className="promotionnal-banner-small__img-container">
+                        <img className="promotionnal-banner-small__img" src={card} alt="card"/>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <h2 className="heading-secondary">Inspire Yourself with Tutorials</h2>
+                <div className="tutorial-banner">
+                    <img className="tutorial-banner__image" src={flowers} alt="flowers"/>
+                    <span className="tutorial-banner__header">Gardening<br/>tips & tricks</span>
+                    <div className="tutorial-banner__description">
+                        <p>6 promoted product</p>
+                        <p>2 min read</p>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 }
