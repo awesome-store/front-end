bf4053a00a5da35d9ccfa8d50b556b6626ba269e
Sergey <in43sh@gmail.com>
2020-06-16 14:15:16 -0700

    Work on faq text container.

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import avatar from '../img/avatar.png';
+import femaleAvatar from '../img/female-avatar.png';
+import { ExternalLink } from 'react-external-link';
 
 function Faq() {
     return (
@@ -45,13 +46,17 @@ function Faq() {
                             <div className="faq-contact-form__contacts-container">
                                 <h3 className="faq-contact-form__header">Or contact us directly:</h3>
                                 <div className="faq-contact-form__image-container">
-                                    <img className="faq-contact-form__image" src={avatar} alt="avatar"/>
+                                    <img className="faq-contact-form__image" src={femaleAvatar} alt="avatar"/>
                                 </div>
                                 <p className="faq-contact-form__contact-person">DANA CHITANIS</p>
                                 <p className="faq-contact-form__contact-info">Customer Service</p>
-                                <p>(800) 123-45-67</p>
-                                <p>(800) 123-45-67</p>
-                                <p>mail@mail.com</p>
+                                <div className="faq-contact-form__contact-info">
+                                    <p>(800) 123-45-67</p>
+                                    <p>(800) 123-45-67</p>
+                                </div>
+                                <ExternalLink className="faq-contact-form__email-link" href="mailto:mail@mail.com">
+                                    <span>mail@mail.com</span>
+                                </ExternalLink>
                             </div>
                         </div>
                     </div>
