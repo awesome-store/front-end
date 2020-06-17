5e3f489e4be176ad031b879c086dba718450ae10
Sergey <in43sh@gmail.com>
2020-06-16 19:21:11 -0700

    Work on faq page.

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import femaleAvatar from '../img/female-avatar.png';
+import caret from '../img/icons/caret-down.svg';
 import { ExternalLink } from 'react-external-link';
 
 function Faq() {
@@ -9,26 +10,53 @@ function Faq() {
                 <div className="faq">
                     <h1 className="heading-primary">FAQ</h1>
                     <div className="faq__items">
-                        <div>
-                            <p className="faq__item-container pointer">How do I request installation/measurement services?</p>
+                        <div className="faq__item-container pointer">
+                            <div className="faq__item-top">
+                                <p>How do I request installation/measurement services?</p>
+                                <img className="caret" src={caret} alt="caret down"/>
+                            </div>
+                            <div className="faq__item-bottom">
+                                <br/>
+                                <p>Cauliflower cheese pecorino cheese on toast. Rubber cheese queso danish fontina red leicester macaroni cheese lancashire who moved my cheese cheese on toast. Port-salut bavarian bergkase hard cheese cow hard cheese parmesan swiss lancashire. Manchego emmental cheesy feet.</p>
+                                <br/>
+                                <p>Babybel fondue stilton. Cheesy grin cheese slices chalk and cheese boursin mascarpone roquefort dolcelatte blue castello. Airedale say cheese camembert de normandie babybel boursin danish fontina babybel who moved my cheese. Queso say cheese.</p>
+                            </div>
                         </div>
-                        <div>
-                            <p className="faq__item-container pointer">How do I make and verify payments?</p>
+                        <div className="faq__item-container pointer">
+                            <div className="faq__item-top">
+                                <p>How do I make and verify payments?</p>
+                                <img className="caret" src={caret} alt="caret down"/>
+                            </div>
                         </div>
-                        <div>
-                            <p className="faq__item-container pointer">Where can I find my bill information?</p>
+                        <div className="faq__item-container pointer">
+                            <div className="faq__item-top">
+                                <p>Where can I find my bill information?</p>
+                                <img className="caret" src={caret} alt="caret down"/>
+                            </div>
                         </div>
-                        <div>
-                            <p className="faq__item-container pointer">How do I check my order status?</p>
+                        <div className="faq__item-container pointer">
+                            <div className="faq__item-top">
+                                <p>How do I check my order status?</p>
+                                <img className="caret" src={caret} alt="caret down"/>
+                            </div>
                         </div>
-                        <div>
-                            <p className="faq__item-container pointer">What's your policy on shipping?</p>
+                        <div className="faq__item-container pointer">
+                            <div className="faq__item-top">
+                                <p>What's your policy on shipping?</p>
+                                <img className="caret" src={caret} alt="caret down"/>
+                            </div>
                         </div>
-                        <div>
-                            <p className="faq__item-container pointer">Whats' your policy on refunds?</p><p className="faq__item-container pointer">Whats' your policy on refunds?</p>
+                        <div className="faq__item-container pointer">
+                            <div className="faq__item-top">
+                                <p>Whats' your policy on refunds?</p>
+                                <img className="caret" src={caret} alt="caret down"/>                                  
+                            </div>
                         </div>
-                        <div>
-                        <p className="faq__item-container pointer">What's your policy on donations to charities?</p>
+                        <div className="faq__item-container faq__item-container--last pointer">
+                            <div className="faq__item-top">
+                                <p>What's your policy on donations to charities?</p>
+                                <img className="caret" src={caret} alt="caret down"/>       
+                            </div>
                         </div>
                     </div>
                     <div className="faq-contact-form">
