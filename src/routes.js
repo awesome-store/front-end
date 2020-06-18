71d7e7649ee89fa2232e71a8bb344ec6c4bbc1fb
Sergey <in43sh@gmail.com>
2020-06-18 02:38:44 -0700

    Add NotFound component.

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Faq from './components/pages/Faq';
 import Account from './components/pages/Account';
+import NotFound from './components/pages/NotFound';
 
 export default (
     <Switch>
         <Route exact path="/" component={ Home }/>
         <Route path="/faq" component={ Faq }/>
         <Route path="/account" component={ Account }/>
+        <Route path="*" component={ NotFound }/>
     </Switch>
 )
