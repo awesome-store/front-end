4a4c5f88d472aaa963f22ddf24e4f7d64e938444
Sergey <in43sh@gmail.com>
2020-06-18 00:49:48 -0700

    Add ContactSupport component.

---
+++ src/components/account/ContactSupport.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ContactSupport() {
+    return (
+        <div>
+            <h2>Contact Support</h2>
+        </div>
+    )
+}
+
+export default ContactSupport;
