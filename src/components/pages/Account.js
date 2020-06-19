69203a91f40615517b776b55515c948846e08687
Sergey <in43sh@gmail.com>
2020-06-18 22:15:15 -0700

    Make sidebar tabs able to switch when click.

--- src/components/pages/Account.js
+++ src/components/pages/Account.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Orders from '../account/Orders';
-// import EditAccount from '../account/EditAccount';
-// import ContactSupport from '../account/ContactSupport';
+import EditAccount from '../account/EditAccount';
+import ContactSupport from '../account/ContactSupport';
 
 function Account() {
+    const [currentTab, setCurrentTab] = useState({value: "Orders"});
+
+    const switchTab = (str) => {
+        switch (str) {
+            case "Edit Account":
+                setCurrentTab({ value: "Edit Account"});
+                break;
+
+            case "Contact Support":
+                setCurrentTab({ value: "Contact Support"});
+                break;
+        
+            default:
+                setCurrentTab({ value: "Orders"});
+                break;
+        }
+    }
     return (
         <div className="wrapper">
             <div className="wrapper__inside-container">
@@ -12,12 +29,20 @@ function Account() {
                     <div className="account__account-wrapper">
                         <nav className="account__account-sidebar">
                             <ul className="account-sidebar__sidebar-list">
-                                <li className="account-sidebar__item pointer">Orders</li>
-                                <li className="account-sidebar__item pointer">Edit Profile</li>
-                                <li className="account-sidebar__item pointer">Contact Support</li>
+                                <li className="account-sidebar__item pointer" onClick={ () => switchTab("Orders" )}>Orders</li>
+                                <li className="account-sidebar__item pointer" onClick={ () => switchTab("Edit Account" )}>Edit Profile</li>
+                                <li className="account-sidebar__item pointer" onClick={ () => switchTab("Contact Support" )}>Contact Support</li>
                             </ul>
                         </nav>
-                        <Orders/>
+                        {
+                            currentTab.value === "Orders"
+                            ? <Orders/>
+                            : currentTab.value === "Edit Account"
+                            ? <EditAccount/>
+                            : currentTab.value === "Contact Support"
+                            ? <ContactSupport/>
+                            : null
+                        }
                     </div>
                 </div>
             </div>
