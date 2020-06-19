28906dcb3d6b53c2ef1abd6387977e985be63c07
Sergey <in43sh@gmail.com>
2020-06-18 21:37:14 -0700

    Work on Orders markup.

--- src/components/account/Orders.js
+++ src/components/account/Orders.js
@@ -4,6 +4,14 @@ function Orders() {
     return (
         <div>
             <h2>My Orders</h2>
+            <div>
+                <h3>Current Orders</h3>
+                <div>
+                    <h3>Status: Shipped</h3>
+                    <h3>Order Number: 235129352</h3>
+                    
+                </div>
+            </div>
         </div>
     )
 }
