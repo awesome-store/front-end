f6eb8f9a8aca923d502eba156d1bff680eb04993
Sergey <in43sh@gmail.com>
2020-06-21 04:06:01 -0700

    Work on Account page markup.

--- src/components/account/EditAccount.js
+++ src/components/account/EditAccount.js
@@ -3,33 +3,33 @@ import React from 'react';
 function EditAccount() {
     return (
         <div className="account__tab-container">
-            <h2 className="heading-primary">Edit My Account</h2>
-            <div className="input-container">
+            <h2 className="account__heading-primary heading-primary">Edit My Account</h2>
+            <div className="account__input-container input-container">
                 <label className="label">E-MAIL:</label>
                 <input className="input"/>
             </div>
-            <div className="input-container">
+            <div className="account__input-container input-container">
                 <label className="label">PASSWORD:</label>
                 <input className="input"/>
             </div>
-            <h2 className="heading-secondary">Shipping Address</h2>
-            <div className="input-container">
+            <h2 className="account__heading-primary heading-secondary">Shipping Address</h2>
+            <div className="account__input-container input-container">
                 <label className="label">NAME:</label>
                 <input className="input"/>
             </div>
-            <div className="input-container">
+            <div className="account__input-container input-container">
                 <label className="label">STREET:</label>
                 <input className="input"/>
             </div>
-            <div className="input-container">
+            <div className="account__input-container input-container">
                 <label className="label">CITY:</label>
                 <input className="input"/>
             </div>
-            <div className="input-container">
+            <div className="account__input-container input-container">
                 <label className="label">STATE:</label>
                 <input className="input"/>
             </div>
-            <div className="input-container">
+            <div className="account__input-container input-container">
                 <label className="label">ZIP CODE:</label>
                 <input className="input"/>
             </div>
