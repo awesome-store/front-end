3b33bc4b958aaffbbd0d1e7e98d4aa744d9b0fd2
Sergey <in43sh@gmail.com>
2020-06-22 01:04:50 -0700

    Work on EditAccount page.

--- src/components/account/EditAccount.js
+++ src/components/account/EditAccount.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function EditAccount() {
     return (
@@ -9,35 +10,42 @@ function EditAccount() {
                     <label className="label">E-MAIL</label>
                     <input className="input"/>
                 </div>
+                <div className="account__change-container">
+                    <Link className="account__change-link">
+                        <span className="account__change-text">Change</span>
+                    </Link>
+                </div>
                 <div className="account__input-container input-container">
                     <label className="label">PASSWORD</label>
                     <input className="input"/>
                 </div>
             </div>
-            <h2 className="account__heading-primary heading-secondary">Shipping Address</h2>
-            <div className="account__input-container input-container">
-                <label className="label">NAME</label>
-                <input className="input"/>
-            </div>
-            <div className="account__input-container input-container">
-                <label className="label">STREET</label>
-                <input className="input"/>
-            </div>
-            <div className="account__input-container input-container">
-                <label className="label">CITY</label>
-                <input className="input"/>
-            </div>
-            <div className="account__input-container input-container">
-                <label className="label">STATE</label>
-                <input className="input"/>
-            </div>
-            <div className="account__input-container input-container">
-                <label className="label">ZIP CODE</label>
-                <input className="input"/>
-            </div>
-            <div className="input-container">
-                <label className="label">COUNTRY</label>
-                <input className="input"/>
+            <h2 className="account__heading-primary heading-primary">Shipping Address</h2>
+            <div className="account__account-info">
+                <div className="account__input-container input-container">
+                    <label className="label">NAME</label>
+                    <input className="input"/>
+                </div>
+                <div className="account__street-input-container account__input-container input-container">
+                    <label className="label">STREET</label>
+                    <input className="input"/>
+                </div>
+                <div className="account__input-container input-container">
+                    <label className="label">CITY</label>
+                    <input className="input"/>
+                </div>
+                <div className="account__input-container input-container">
+                    <label className="label">STATE</label>
+                    <input className="input"/>
+                </div>
+                <div className="account__input-container input-container">
+                    <label className="label">ZIP CODE</label>
+                    <input className="input"/>
+                </div>
+                <div className="account__input-container input-container">
+                    <label className="label">COUNTRY</label>
+                    <input className="input"/>
+                </div>
             </div>
         </div>
     )
