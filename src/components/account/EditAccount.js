f3bd1fb308c266034173634a0b60bafae13f8ab5
Sergey <in43sh@gmail.com>
2020-06-21 21:53:39 -0700

    Work on EditAccount input containers.

--- src/components/account/EditAccount.js
+++ src/components/account/EditAccount.js
@@ -4,37 +4,39 @@ function EditAccount() {
     return (
         <div className="account__tab-container">
             <h2 className="account__heading-primary heading-primary">Edit My Account</h2>
-            <div className="account__input-container input-container">
-                <label className="label">E-MAIL:</label>
-                <input className="input"/>
-            </div>
-            <div className="account__input-container input-container">
-                <label className="label">PASSWORD:</label>
-                <input className="input"/>
+            <div className="account__change-email-container">
+                <div className="account__input-container input-container">
+                    <label className="label">E-MAIL</label>
+                    <input className="input"/>
+                </div>
+                <div className="account__input-container input-container">
+                    <label className="label">PASSWORD</label>
+                    <input className="input"/>
+                </div>
             </div>
             <h2 className="account__heading-primary heading-secondary">Shipping Address</h2>
             <div className="account__input-container input-container">
-                <label className="label">NAME:</label>
+                <label className="label">NAME</label>
                 <input className="input"/>
             </div>
             <div className="account__input-container input-container">
-                <label className="label">STREET:</label>
+                <label className="label">STREET</label>
                 <input className="input"/>
             </div>
             <div className="account__input-container input-container">
-                <label className="label">CITY:</label>
+                <label className="label">CITY</label>
                 <input className="input"/>
             </div>
             <div className="account__input-container input-container">
-                <label className="label">STATE:</label>
+                <label className="label">STATE</label>
                 <input className="input"/>
             </div>
             <div className="account__input-container input-container">
-                <label className="label">ZIP CODE:</label>
+                <label className="label">ZIP CODE</label>
                 <input className="input"/>
             </div>
             <div className="input-container">
-                <label className="label">COUNTRY:</label>
+                <label className="label">COUNTRY</label>
                 <input className="input"/>
             </div>
         </div>
