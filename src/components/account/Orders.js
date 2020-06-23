1fe2eacf71c6eb2f0d3ffd99d02f6f4d0aa3e7a9
Sergey <in43sh@gmail.com>
2020-06-23 01:26:53 -0700

    Work on Orders component.

--- src/components/account/Orders.js
+++ src/components/account/Orders.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 function Orders() {
     return (
-        <div>
+        <div className="account__tab-container">
             <h2 className="account__heading-primary heading-primary">My Orders</h2>
-            <div>
-                <h3>Current Orders</h3>
-                <div>
-                    <h3>Status: Shipped</h3>
-                    <h3>Order Number: 235129352</h3>
+            <div className="account__orders-container">
+                <div className="account__order-item">
+                    <p>Item</p>
                 </div>
             </div>
         </div>
