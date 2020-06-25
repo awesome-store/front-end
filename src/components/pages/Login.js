185f8c1364ff8591da3e598be78e7824427acb12
Sergey <in43sh@gmail.com>
2020-06-25 03:02:47 -0700

    Work on Login page.

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import fb from '../../img/icons/fb.svg';
+import fb from '../../img/icons/fb-white.svg';
+import { Link } from 'react-router-dom';
 
 function Login() {
     return (
         <div className="login-wrapper">
             <div className="login">
-                <h1>Login</h1>
-                <div className="login__login-with-fb">
-                    <img className="login-with-fb__facebook-btn" src={fb} alt="facebook"/>
-                    <p className="login-with-fb__text">Log in with Facebook</p>
+                <h1 className="login__heading">Login</h1>
+                <div className="login__facebook-btn pointer">
+                    <img className="login__facebook-img" src={fb} alt="facebook"/>
+                    <p>Log in with Facebook</p>                    
+                </div>
+                <p>OR</p>
+                <input className="input"/>
+                <input className="input"/>
+                <Link>
+                    <p>Restore password</p>
+                </Link>
+                <div>
+                    <p>Log in</p>
                 </div>
             </div>
         </div>
