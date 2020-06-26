7f3e5209d494328e4e8f50a2f312076e042d2426
Sergey <in43sh@gmail.com>
2020-06-26 00:23:00 -0700

    Work on tutorials top screen.

--- src/components/pages/Tutorials.js
+++ src/components/pages/Tutorials.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 function Tutorials() {
     return (
-        <div className="wrapper">
-            <div className="wrapper__inside-container">
-                <p>Tutorials</p>
+        <div className="tutorials">
+            <div className="tutorials__top-screen">
+                <div className="tutorials__top-screen-text-container">
+                    <h1 className="tutorials__heading">DIY Home Projects</h1>
+                    <p className="tutorials__top-screen-text">A DIY Home Project is the easiest way to liven up your space. We have DIY projects for every room in the house including the bedroom, kitchen, bathroom, and living room, not to mention outdoor</p>
+                </div>
+            </div>
+            <div className="wrapper">
+                <div className="wrapper__inside-container">
+                    <p>Tutorials</p>
+                </div>
             </div>
         </div>
     )
