84b0ba72320dcad3760c013145ff79b0d1ccc97a
Sergey <in43sh@gmail.com>
2020-07-03 23:52:53 -0700

    Work on Category and Categories content: pass props to Category, add Link to Category.

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Category(props) {
     return (
-        <div className="categories__item">
-            <img className="img-fluid" src={props.img} alt={props.img}/>
-            <p>{props.title}</p>
-        </div>
+        <Link to={`/store/${props.url}`}>
+            <div className="categories__item">
+                <img className="img-fluid" src={props.img} alt={props.img}/>
+                <p>{props.title}</p>
+            </div>
+        </Link>
     )
 }
 
