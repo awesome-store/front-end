6084038a1fc2e7c180f59180bd0f3ca486b21892
Sergey <in43sh@gmail.com>
2020-07-07 01:19:39 -0700

    Work on sidebar: add cross, make active item-container, active item.

--- src/components/products/CategoryProducts.js
+++ src/components/products/CategoryProducts.js
@@ -4,6 +4,7 @@ import drill from "../../img/drill.png";
 import paint from "../../img/paint.png";
 import screwdrivers from "../../img/screwdrivers.png";
 import toolkit from "../../img/toolkit.png";
+import cross from "../../img/icons/cross.svg";
 
 function CategoryProducts(props) {
     return (
@@ -16,30 +17,93 @@ function CategoryProducts(props) {
                 <div className="category-products__category-products-sidebar">
                     <div className="category-products-sidebar__property-group">
                         <p className="category-products-sidebar__property-heading pointer">BRAND</p>
-                        <p className="category-products-sidebar__property-item pointer">Any</p>
-                        <p className="category-products-sidebar__property-item category-products-sidebar__property-item--active pointer">Behr x</p>
-                        <p className="category-products-sidebar__property-item pointer">PPG</p>
-                        <p className="category-products-sidebar__property-item pointer">Glidden</p>
-                        <p className="category-products-sidebar__property-item pointer">Rust-Oleum</p>
-                        <p className="category-products-sidebar__property-item pointer">Kitz</p>
-                        <p className="category-products-sidebar__property-item pointer">Wagner</p>
-                        <p className="category-products-sidebar__property-item pointer">Graco</p>
-                        <p className="category-products-sidebar__property-item pointer">Plastidip</p>
-                        <p className="category-products-sidebar__property-item pointer">3M</p>
-                        <p className="category-products-sidebar__property-item pointer">Wooster Pro</p>
-                        <p className="category-products-sidebar__property-item pointer">Varathane</p>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Any</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container category-products-sidebar__property-item-container--active">
+                            <p className="category-products-sidebar__property-item category-products-sidebar__property-item--active pointer">Behr</p>
+                            <img className="category-products-sidebar__property-item-cross category-products-sidebar__property-item-cross--active pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">PPG</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Glidden</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Rust-Oleum</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Kitz</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Wagner</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Graco</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Plastidip</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">3M</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Wooster Pro</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Varathane</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
                     </div>
                     <div className="category-products-sidebar__property-group">
                         <p className="category-products-sidebar__property-heading">TYPE</p>
-                        <p className="category-products-sidebar__property-item pointer">Any</p>
-                        <p className="category-products-sidebar__property-item pointer">Interior Paint</p>
-                        <p className="category-products-sidebar__property-item pointer">Exterior Paint</p>
-                        <p className="category-products-sidebar__property-item pointer">Primer</p>
-                        <p className="category-products-sidebar__property-item pointer category-products-sidebar__property-item--active">Ceiling Paint x</p>
-                        <p className="category-products-sidebar__property-item pointer">Spray</p>
-                        <p className="category-products-sidebar__property-item pointer">Concrete & Garage Floor</p>
-                        <p className="category-products-sidebar__property-item pointer">Masonry & Brick</p>
-                        <p className="category-products-sidebar__property-item pointer">Roof Paint</p>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Any</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />  
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Interior Paint</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />   
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Exterior Paint</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />  
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Primer</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container category-products-sidebar__property-item-container--active">
+                            <p className="category-products-sidebar__property-item pointer category-products-sidebar__property-item--active">Ceiling Paint</p> 
+                            <img className="category-products-sidebar__property-item-cross category-products-sidebar__property-item-cross--active pointer" src={cross} />   
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Spray</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} /> 
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Concrete & Garage Floor</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Masonry & Brick</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
+                        <div className="category-products-sidebar__property-item-container">
+                            <p className="category-products-sidebar__property-item pointer">Roof Paint</p>
+                            <img className="category-products-sidebar__property-item-cross pointer" src={cross} />
+                        </div>
                     </div>
                 </div>
                 <Product img={drill}/>
