bf6e21b703796066917da41b49ff8b1825cfa10a
Sergey <in43sh@gmail.com>
2020-07-09 04:33:36 -0700

    Add Product page component.

---
+++ src/components/pages/Product.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Product() {
+    return (
+        <div className="wrapper">
+            <div className="wrapper__inside-container">
+                <p>Product</p>
+            </div>
+        </div>
+    )
+}
+
+export default Product;
