07a9a0b04605c434b036810d54c195e34daa1ea9
Sergey <in43sh@gmail.com>
2020-07-12 03:12:23 -0700

    Work on Product component.

--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -3,22 +3,30 @@ import YouMightNeedProducts from "../products/YouMightNeedProducts"
 import WeDeliverBanner from '../banners/WeDeliverBanner';
 import CategoriesLinksHome from '../CategoriesLinksHome';
 import Reviews from '../Reviews';
+import paint from '../../img/paint.png';
 
 function Product() {
     return (
         <div>
             <div className="wrapper">
                 <div className="wrapper__inside-container">
-                    <div className="breadcrumbs-container product__breadcrumgs-container align-self-start">
+                    <div className="breadcrumbs-container product-page__breadcrumbs-container align-self-start">
                         <p className="breadcrumbs-container__item breadcrumbs-container__item--parent pointer">Home</p>
                         <p className="breadcrumbs-container__arrow">→</p>
                         <p className="breadcrumbs-container__item breadcrumbs-container__item--parent pointer">Store</p>
                         <p className="breadcrumbs-container__arrow">→</p>
                         <p className="breadcrumbs-container__item">Paint</p>
                     </div>
-                    <div className="product"></div>
-                    <Reviews className="product__reviews"/>
-                    <YouMightNeedProducts className="product__you-might-need-products"/>
+                    <div className="product">
+                        <div className="product__top">
+                            <p className="product__title">Behr Premium Oak Hans Wegner</p>
+                        </div>
+                        <div>
+                            <img src={paint} alt="paint"/>
+                        </div>
+                    </div>
+                    <Reviews className="product-page__reviews"/>
+                    <YouMightNeedProducts className="product-page__you-might-need-products"/>
                 </div>
             </div>
             <WeDeliverBanner/>
