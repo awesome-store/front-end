21b151c3915ac8a2c3779eda96055dff201be95b
Sergey <in43sh@gmail.com>
2020-07-11 21:44:31 -0700

    Try to pass props through Reviews.js .

--- src/components/Review.js
+++ src/components/Review.js
@@ -3,12 +3,20 @@ import avatar from '../img/avatar.png';
 import goldstar from "../img/icons/gold-star.svg";
 import star from "../img/icons/star.svg";
 
-function Review() {
+function Review(props) {
+    let test = () => {
+        console.log(props.name.first);
+    }
+
     return (
         <div className="review">
             <div className="review__top">
                 <img src={avatar} alt="avatar"/>
                 <div className="review__review-author-container">
+                    {/* <p>{props.name.first}</p>
+                    <p>{props.name.last}</p> */}
+                    {/* <p>{props.name.first}</p> */}
+                    {/* <p>{props.name}</p> */}
                     <p>Chomkwan</p>
                     <p>Wattana</p>
                 </div>
@@ -23,6 +31,9 @@ function Review() {
                     <img src={star} alt="star"/>
                 </div>
                 <p>Cheddar parmesan cream cheese. Babybel boursin chalk and cheese cottage cheese croque monsieur pepper jack hard cheese cheese and biscuits. Red leicester cheddar macaroni cheese goat lancashire croque monsieur boursin cream cheese. When the cheese comes out everybody's happy cheesy grin stilton mascarpone cheese strings boursin.</p>
+            </div>
+            <div onClick={test}>
+                <p>Click</p>
             </div>
         </div>
     )
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Review from './Review';
 
-function Reviews() {
+function Reviews(props) {
+    // let obj = {first: "Chomkwan", last: "Wattana"};
     return (
         <div className="reviews-container">
             <h1 className="heading-primary">Reviews</h1>
             <div className="reviews">
+                <Review name={ {first: "Chomkwan", last: "Wattana"} }/>
+                {/* <Review name="name"/> */}
+                {/* <Review name={obj}/> */}
                 <Review/>
                 <Review/>
                 <Review/>
