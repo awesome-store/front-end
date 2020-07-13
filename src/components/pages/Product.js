84c104031e9665397507804666c3c5df4bb48d97
Sergey <in43sh@gmail.com>
2020-07-12 17:24:38 -0700

    Work on Product markup.

--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -4,6 +4,9 @@ import WeDeliverBanner from '../banners/WeDeliverBanner';
 import CategoriesLinksHome from '../CategoriesLinksHome';
 import Reviews from '../Reviews';
 import paint from '../../img/paint.png';
+import paintInUse1 from '../../img/paint-in-use1.png';
+import paintInUse2 from '../../img/paint-in-use2.png';
+import paintInUse3 from '../../img/paint-in-use3.png';
 
 function Product() {
     return (
@@ -19,10 +22,29 @@ function Product() {
                     </div>
                     <div className="product">
                         <div className="product__top">
-                            <p className="product__title">Behr Premium Oak Hans Wegner</p>
+                            <div>
+                                <p className="product__title">Behr Premium Oak Hans Wegner</p>
+                                <div>
+                                    <p>$29.99</p>
+                                    <p>/ gal</p>
+                                </div>
+                                <div>
+
+                                </div>
+                            </div>
+                            <div>
+                                <img src={paint} alt="paint"/>
+                            </div>
                         </div>
                         <div>
-                            <img src={paint} alt="paint"/>
+                            <p>Cheese and wine pecorino roquefort. Cottage cheese cottage cheese st. agur blue cheese paneer macaroni cheese boursin bavarian bergkase caerphilly. Goat cauliflower cheese cheese and biscuits cauliflower cheese emmental cheeseburger macaroni cheese pepper jack. Taleggio st. agur blue cheese cottage cheese.
+
+                            Lancashire stinking bishop caerphilly. Manchego taleggio parmesan cheese and wine roquefort cream cheese emmental danish fontina. Squirty cheese fondue pepper jack cheese and wine cheese slices mascarpone pepper jack emmental. Fromage frais.</p>
+                            <div className="product__use-images">
+                                <img src={paintInUse1} alt="paint in use 1"/>
+                                <img src={paintInUse2} alt="paint in use 2"/>
+                                <img src={paintInUse3} alt="paint in use 3"/>
+                            </div>
                         </div>
                     </div>
                     <Reviews className="product-page__reviews"/>
