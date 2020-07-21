1bcae33f209aa79d71b09823a4b0b07ffb04c6c3
Sergey <in43sh@gmail.com>
2020-07-21 03:21:32 -0700

    Start working on cart and cart-row.

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import paint from '../../img/paint.png';
 
 function Cart() {
     return (
@@ -6,7 +7,19 @@ function Cart() {
             <div className="wrapper__inside-container">
                 <h1 className="heading-primary">Shopping Cart</h1>
                 <p>3 items</p>
-                <div className="cart"></div>
+                <div className="cart">
+                    <div className="cart-row">
+                        <p>Name</p>
+                        <p>Price</p>
+                        <p>Quantity</p>
+                        <p></p>
+                    </div>
+                    <div className="cart-row">
+                        <div className="cart-row__product-info-container">
+                            <img className="img-fluid" src={paint} alt="paint"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
