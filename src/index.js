6ac4d28bbd99543fca755adb5af3873cf05bfeac
Sergey <in43sh@gmail.com>
2020-08-12 04:26:31 -0700

    Decide to use HashRouter as it doesn't break the front end.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { HashRouter as Router } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
 import './scss/main.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
