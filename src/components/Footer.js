c7816949dcbb05268f72bff25352b3c7aef87121
Sergey <in43sh@gmail.com>
2020-08-30 20:08:08 -0700

    Change logo picture.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ExternalLink } from 'react-external-link';
-import logo from '../img/logo.png';
+import logo from '../img/logo-black.svg';
 import facebook from '../img/icons/facebook.svg';
 import twitter from '../img/icons/twitter.svg'
 import instagram from '../img/icons/instagram.svg';
