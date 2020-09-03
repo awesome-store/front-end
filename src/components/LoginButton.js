d616b2dfe4fc893ddff0adb8e47783c340aac408
Sergey <in43sh@gmail.com>
2020-09-03 03:41:27 -0700

    Fix bugs with removing Autho0.

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+// import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
+    // const { loginWithRedirect } = useAuth0();
 
     return (
-        <div className="login__log-in-btn btn btn--yellow pointer" onClick= {() => loginWithRedirect() }>
+        <div className="login__log-in-btn btn btn--yellow pointer">
             <p>Log in</p>
         </div>
     )
