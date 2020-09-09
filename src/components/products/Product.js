25ea319a047773c2c28c8e48e77ef6665b4b3406
Sergey <in43sh@gmail.com>
2020-09-09 10:15:00 -0700

    Work on Product component: make Product info console.log .

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import goldstar from "../../img/icons/gold-star.svg";
 import star from "../../img/icons/star.svg";
 
 function Product(props) {
+    const [productInfo, setProductInfo] = useState({
+        image: "https://source.unsplash.com/random",
+        productTitle: "Ryobi Lithium-Ion Cordless Drill + Charger",
+        price: "49.99",
+        unit: "piece",
+        rating: "4"
+    })
+
+    const addToCart = () => {
+        console.log(productInfo);
+    }
+
     return (
         <div className="product-cards__product-card">
             <div className="product-card__img-container">
@@ -15,12 +27,12 @@ function Product(props) {
                 <img src={goldstar} alt="goldstar"/>
                 <img src={star} alt="star"/>
             </div>
-            <span className="product-card__heading">Ryobi Lithium-Ion<br/>Cordless Drill + Charger</span>
+            <span className="product-card__heading">{productInfo.productTitle}</span>
             <div className="product-card__price-container">
-                <span className="product-card__price">$49.99</span>
-                <span className="product-card__price-unit">/ piece</span>
+                <span className="product-card__price">${productInfo.price}</span>
+                <span className="product-card__price-unit">/ {productInfo.unit}</span>
             </div>
-            <div className="product-card__button pointer">
+            <div className="product-card__button pointer" onClick={() => addToCart()}>
                 <span>Add to Cart</span>
             </div>
         </div>
