14aa92b547a00331b987facdc35841edade05ac8
Sergey <in43sh@gmail.com>
2020-09-08 23:47:16 -0700

    Add redux components.

---
+++ src/redux/reducer.js
@@ -0,0 +1,39 @@
+// initial state
+const initialState = {
+    user: null,
+    url: null
+  }
+  // action type 
+  const LOGIN = 'LOGIN';
+  const URLSEND = 'URLSEND';
+  
+  // action creators
+  export const login = (user) => {
+    return {
+      type: LOGIN,
+      payload: user
+    };
+  };
+  
+  export const urlsend = (url) => {
+    return {
+      type: URLSEND,
+      payload: url
+    };
+  };
+  
+  // reducer
+  const reducer = (state = initialState, action) => {
+    // const { type, payload } = action
+    switch (action.type) {
+      case LOGIN:
+        return { ...state, user: action.payload };
+  
+      case URLSEND:
+        return { ...state, url: action.payload };
+  
+      default: return state;
+    }
+  }
+  
+  export default reducer;
