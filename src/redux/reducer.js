ebe98e37d2b992e5a9ddf08bb7a4215fb91cf6b0
Sergey <in43sh@gmail.com>
2020-09-16 00:03:34 -0700

    Make reducer add some value to initialState object.

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,77 +1,80 @@
 // initial state
-const initialState = {
-    cart: [
-      {id:1,title:'Winter body', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110},
-      {id:2,title:'Adidas', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:80},
-      {id:3,title:'Vans', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:120},
-      {id:4,title:'White', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:260},
-      {id:5,title:'Cropped-sho', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:160},
-      {id:6,title:'Blues', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:90}
-    ],
-    addedItems: [],
-    total: 0
-  }
-  // action type 
-  const ADDTOCART = 'ADDTOCART';
-  const REMOVEFROMCART = 'REMOVEFROMCART';
-  
-  // action creators
-  export const addtocart = (cart) => {
-    return {
-      type: ADDTOCART,
-      payload: cart
-    };
-  };
-  
-  // export const removefromcart = (cart) => {
-  //   return {
-  //     type: REMOVEFROMCART,
-  //     payload: cart
-  //   };
-  // };
-  
-  // reducer
-  const reducer = (state = initialState, action) => {
-    // const { type, payload } = action
-    // switch (action.type) {
-    //   case ADDTOCART:
-    //     return { ...state, cart: action.payload };
-  
-    //   case REMOVEFROMCART:
-    //     return { ...state, cart: action.payload };
-  
-    //   default: return state;
-    // }
-    if (action.type === ADDTOCART) {
-      console.log("here======>")
-      console.log("state.cart ", state.cart);
-      console.log("state.cart[0]", state.cart[0]);
-      console.log("action ", action)
-      // if(Object.keys(x).length > 0){
-      // if(state.cart)
-      let addedItem = state.cart.find(item => item.id === action.payload.id)
-      console.log(addedItem);
-      let existedItem = state.addedItems.find(item => action.id === item.id)
-      if (existedItem) {
-        addedItem.quantity += 1
-        return {
-          ...state,
-          total: state.total + addedItem.price
-        }
-      } else {
-        console.log(addedItem);
-        addedItem.quantity = 1;
-        let newTotal = state.total + addedItem.price
-
-        return {
-          ...state,
-          addedItems: [...state.addedItems, addedItem],
-          total: newTotal
-        }
-      }
-    } else {
-      return state;
+let initialState = {
+  cart: {
+    "0": {
+      value: "null",
+      quantity: "null"
     }
+  },
+  total: 0
+}
+
+// action type 
+const ADD_TO_CART = 'ADDTOCART';
+// const REMOVEFROMCART = 'REMOVEFROMCART';
+
+// action creators
+export const addtocart = (cart) => {
+  return {
+    type: ADD_TO_CART,
+    payload: cart
+  };
+};
+
+// export const removefromcart = (cart) => {
+//   return {
+//     type: REMOVEFROMCART,
+//     payload: cart
+//   };
+// };
+
+// reducer
+const reducer = (state = initialState, action) => {
+  const { type, payload } = action
+  // switch (type) {
+  //   case ADDTOCART:
+  //     return { ...state, cart: payload };
+
+  //   case REMOVEFROMCART:
+  //     return { ...state, cart: payload };
+
+  //   default: return state;
+  // }
+  if (type === ADD_TO_CART) {
+    console.log("here======>")
+    console.log("state.cart ", state.cart);
+    console.log("state.cart[0]", state.cart[0]);
+    console.log("action ", action)
+    state.cart["test"] = action.payload;
+    console.log("state.cart['test']", state.cart["test"]);
+    return state;
+    // if(Object.keys(x).length > 0){
+    // if(state.cart)
+    //   let addedItem = state.cart.find(item => item.id === payload.id)
+    //   console.log(addedItem);
+    //   let existedItem = state.addedItems.find(item => id === item.id)
+    //   if (existedItem) {
+    //     addedItem.quantity += 1
+    //     return {
+    //       ...state,
+    //       total: state.total + addedItem.price
+    //     }
+    //   } else {
+    //     console.log(addedItem);
+    //     addedItem.quantity = 1;
+    //     let newTotal = state.total + addedItem.price
+
+    //     return {
+    //       ...state,
+    //       addedItems: [...state.addedItems, addedItem],
+    //       total: newTotal
+    //     }
+    //   }
+    // } else {
+    //   return state;
+    // }
   }
+  return state;
+}
   
 export default reducer;
