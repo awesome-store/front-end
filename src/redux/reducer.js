2a08c6e2d326edb8eeb7de001dfe84fe5bad4213
Sergey <in43sh@gmail.com>
2020-09-18 02:20:01 -0700

    Remove commented out code.

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,13 +1,5 @@
 // initial state
 let initialState = {
-  // cart: {
-  //   "image": "https://source.unsplash.com/random",
-  //   "productTitle": "Ryobi Lithium-Ion Cordless Drill + Charger",
-  //   "price": "49.99",
-  //   "unit": "piece",
-  //   "rating": "4",
-  //   "id": 5
-  // },
   cart: [],
   total: 0
 }
@@ -44,7 +36,6 @@ const reducer = (state = initialState, action) => {
   //   default: return state;
   // }
   if (type === ADD_TO_CART) {
-    console.log("HERE");
     const product = state.cart.find(item => item.id === action.payload.id);
     if (product) {
       const tempProduct = {...product, quantity: product.quantity + 1}
@@ -53,32 +44,7 @@ const reducer = (state = initialState, action) => {
       const tempProduct = {...action.payload, quantity: 1}
       return {...state, cart: [...state.cart, tempProduct]}
     }
-    // for (let key in state.cart) {
-    //   console.log("key ", key);
-    //   if (key === "id" && state.cart[key] === action.payload.id) {
-    //     console.log("here quantity ", state.cart[action.payload.id]["quantity"]);
-    //     // console.log("----key === action.payload.id----");
-    //     state.cart[action.payload.id]["quantity"] += 1
-    //   } else {
-    //     state.cart[action.payload.id] = {
-    //       "value": action.payload,
-    //       "quantity": 0
-    //     }
-    //   }
-    // }
-    // console.log("here======>")
-    // console.log("state.cart ", state.cart);
-    // console.log("state.cart[0]", state.cart[0]);
-    // console.log("action ", action)
-
-    // state.cart[action.payload.id] = {
-    //   "value": action.payload,
-    //   "quantity": 0
-    // }
-    
-    // console.log("action.payload.id", action.payload.id);
-    // console.log("state.cart['test']", state.cart);
-    return state;
+    // return state;
   }
   return state;
 }
