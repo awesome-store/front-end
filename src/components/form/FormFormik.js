fa5a2420020c273bbacd1d530e940cb408361ad5
Sergey <in43sh@gmail.com>
2020-12-24 22:44:22 -0700

    Add content to FormFormik.

--- src/components/form/FormFormik.js
+++ src/components/form/FormFormik.js
@@ -1,5 +1,54 @@
 import React from 'react';
+import { withFormik } from 'formik';
 
-// function FormFormik 
+function MyForm({
+    values,
+    touched,
+    errors,
+    handleChange,
+    handleBlur,
+    handleSubmit
+}) {
+    return (
+        <>
+            <h2>Form</h2>
+            <Form
+                handleBlur={handleBlur}
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+                errors={errors}
+                touched={touched}
+                values={values}
+            />
+            {/* <Debug values={values} errors={errors} touched={touched} /> */}
+        </>
+    )
+}
+
+const FormFormik = withFormik({
+    mapPropsToValues: ({ initialValues }) => {
+        return {
+            ...initialValues
+        };
+    },
+
+    validate: (values, { validate }) => 
+        Object.keys(values).reduce((errors, field) => {
+            const error = validate[field](values[field]);
+            return {
+                ...errors,
+                ...(error && { [filed]: error })
+            };
+        }, {}),
+
+    handleSubmit: (values, { setSubmitting }) => {
+        alert(JSON.stringify(values, null, 2));
+        setSubmitting(false);
+    },
+
+    validateOnChange: false,
+
+    displayName: "FormFormik"
+})(MyForm);
 
 export default FormFormik;
