497fc8be94deb73f6a243bafee242d3e3f8094d1
Sergey <in43sh@gmail.com>
2020-12-26 20:14:07 -0700

    Make sure that Account page doesn't load if the user is not logged in.

--- src/components/pages/Account.js
+++ src/components/pages/Account.js
@@ -56,36 +56,41 @@ function Account() {
         }
     }
     return (
-        <div className="wrapper">
-            <div className="wrapper__inside-container">
-                <div className="account">
-                    <h1 className="account__heading-primary heading-primary">My account</h1>
-                    <div className="account__account-wrapper">
-                        <nav className="account__account-sidebar">
-                            <ul className="account-sidebar__sidebar-list">
-                                <li className="account-sidebar__item pointer" onClick={ () => switchTab("Orders" )}>Orders</li>
-                                <li className="account-sidebar__item pointer" onClick={ () => switchTab("Edit Account" )}>Edit Profile</li>
-                                <li className="account-sidebar__item pointer" onClick={ () => switchTab("Contact Support" )}>Contact Support</li>
-                                <li className="account-sidebar__item pointer" onClick={ () => switchTab("Private Page" )}>Private Page</li>
-                            </ul>
-                        </nav>
-                        <div className="account__content-container">
-                            {
-                                currentTab.value === "Orders"
-                                ? <Orders/>
-                                : currentTab.value === "Edit Account"
-                                ? <EditAccount/>
-                                : currentTab.value === "Contact Support"
-                                ? <ContactSupport/>
-                                : currentTab.value === "Private Page"
-                                ? <PrivatePage/>
-                                : null
-                            }
+        <>
+            {
+                (localStorage.getItem('token')) ?
+                (<div className="wrapper">
+                    <div className="wrapper__inside-container">
+                        <div className="account">
+                            <h1 className="account__heading-primary heading-primary">My account</h1>
+                            <div className="account__account-wrapper">
+                                <nav className="account__account-sidebar">
+                                    <ul className="account-sidebar__sidebar-list">
+                                        <li className="account-sidebar__item pointer" onClick={ () => switchTab("Orders" )}>Orders</li>
+                                        <li className="account-sidebar__item pointer" onClick={ () => switchTab("Edit Account" )}>Edit Profile</li>
+                                        <li className="account-sidebar__item pointer" onClick={ () => switchTab("Contact Support" )}>Contact Support</li>
+                                        <li className="account-sidebar__item pointer" onClick={ () => switchTab("Private Page" )}>Private Page</li>
+                                    </ul>
+                                </nav>
+                                <div className="account__content-container">
+                                    {
+                                        currentTab.value === "Orders"
+                                        ? <Orders/>
+                                        : currentTab.value === "Edit Account"
+                                        ? <EditAccount/>
+                                        : currentTab.value === "Contact Support"
+                                        ? <ContactSupport/>
+                                        : currentTab.value === "Private Page"
+                                        ? <PrivatePage/>
+                                        : null
+                                    }
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
+                </div>) : null
+            }
+        </>
     )
 }
 
