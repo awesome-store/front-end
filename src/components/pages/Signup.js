a19018110a6d52aa50d41853d27122834e20bd1e
Sergey <in43sh@gmail.com>
2020-12-28 23:04:02 -0700

    Add Formik validation to Signup.js.

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import SignupFormik from '../form/SignupForm';
 
 function Signup() {
+    const nameValidation = name => {
+        if (name.trim() === "") {
+            return "Name is required";
+        }
+        return null;
+    }
+
+    const emailValidation = email => {
+        if (email.trim() === "") {
+            return "Email is required";
+        }
+        return null;
+    }
+
+    const passwordValidation = password => {
+        if (!password) {
+            return "Password is required";
+        }
+        if (/[^a-zA-Z -]/.test(password)) {
+            return "Invalid characters";
+        }
+        if (password.length < 4) {
+            return "Password needs to have at least 4 characters";
+        }
+        return null;
+    }
+
+    const validate = {
+        name: nameValidation,
+        email: emailValidation,
+        password: passwordValidation
+    }
+
+    const initialValues = {
+        name: "test",
+        email: "test",
+        password: "test"
+    }
+
     return (
         <div className="login-wrapper">
             <div className="login">
-            <h1 className="login__heading">New Account</h1>
-                <input className="login__input input" placeholder="Name"/>
+                <h1 className="login__heading">New Account</h1>
+                {/* <input className="login__input input" placeholder="Name"/>
                 <input className="login__input input" placeholder="Email"/>
                 <input className="login__input input" placeholder="Password"/>
                 <div className="login__terms-container">
@@ -15,7 +55,8 @@ function Signup() {
                 </div>
                 <div className="login__log-in-btn btn pointer">
                     <p>Sign up</p>
-                </div>
+                </div> */}
+                <SignupFormik validate={validate} initialValues={initialValues}/>
             </div>
         </div>
     )
