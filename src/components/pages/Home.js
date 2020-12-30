a8b230e5ce96efff5bb142ba233a49724460383b
Sergey <in43sh@gmail.com>
2020-12-29 18:32:44 -0700

    Merge conflicts

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -23,30 +23,7 @@ function Home() {
             </div>
             <div className="wrapper">
                 <div className="wrapper__inside-container">
-<<<<<<< HEAD
-                    <section className="home-page__promotional-banner-container">
-                        <div className="promotional-banner-small">
-                            <div  className="promotional-banner-small__text-container promotional-banner-small__text-container--purple">
-                                <span className="promotional-banner-small__header">50% Off Select<br/>Dyson Vacuums</span>
-                                <span className="promotional-banner-small__description">Free delivery. Today only</span>
-                            </div>
-                            <div style={{backgroundImage: "url('https://raw.githubusercontent.com/awesome-store/front-end/master/src/img/vacuum.png')"}} className="promotional-banner-small__img-container">
-                                {/* <img className="promotional-banner-small__img" src={vacuum} alt="vacuum"/> */}
-                            </div>
-                        </div>
-                        <div className="promotional-banner-small">
-                            <div className="promotional-banner-small__text-container promotional-banner-small__text-container--blue">
-                                <span className="promotional-banner-small__header">Financing Available</span>
-                                <span className="promotional-banner-small__description">Up to 24 month financing. Terms & conditions apply</span>
-                            </div>
-                            <div style={{backgroundImage: "url('https://raw.githubusercontent.com/awesome-store/front-end/master/src/img/card.png')"}}  className="promotional-banner-small__img-container">
-                                {/* <img className="promotional-banner-small__img" src={card} alt="card"/> */}
-                            </div>
-                        </div>
-                    </section>
-=======
                     <PromotionalBanners className="home__promotional-banners"/>
->>>>>>> dev
                 </div>
             </div>
             <div className="wrapper home__tutorial-banner-wrapper">
