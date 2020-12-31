de58e3a9c0b9e6d3d4e8615fb5071d7d167066df
Sergey <in43sh@gmail.com>
2020-12-31 00:43:12 -0700

    Add a Loader component.

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import loaderGif from "../img/loader.gif";
+
+function Loader() {
+    return (
+        <>
+            <img src={loaderGif}/>
+        </>
+    )
+}
+
+export default Loader;
